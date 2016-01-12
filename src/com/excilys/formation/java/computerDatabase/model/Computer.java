616de3cfab309eca3099a829504d71f89668d02f
TimotheePapin <papin.timothee@gmail.com>
2016-01-12 14:35:15 +0100

    Fixed models

--- src/com/excilys/formation/java/computerDatabase/model/Computer.java
+++ src/com/excilys/formation/java/computerDatabase/model/Computer.java
@@ -1,23 +1,26 @@
 package com.excilys.formation.java.computerDatabase.model;
 
+import java.time.LocalDate;
+
 public class Computer {
 
 	private int id;
 	private String name;
-	private String introduced;
-	private String discontinued;
-	private int companyId;
+	private LocalDate introduced;
+	private LocalDate discontinued;
+	private String company;
 
 	public Computer() {
-
+		super();
 	}
 
-	public Computer(int id, String name, String introduced, String discontinued, int companyId) {
+	public Computer(int id, String name, LocalDate introduced, LocalDate discontinued, String company) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.introduced = introduced;
 		this.discontinued = discontinued;
-		this.companyId = companyId;
+		this.company = company;
 	}
 
 	public int getId() {
@@ -36,28 +39,79 @@ public class Computer {
 		this.name = name;
 	}
 
-	public String getIntroduced() {
+	public LocalDate getIntroduced() {
 		return introduced;
 	}
 
-	public void setIntroduced(String introduced) {
+	public void setIntroduced(LocalDate introduced) {
 		this.introduced = introduced;
 	}
 
-	public String getDiscontinued() {
+	public LocalDate getDiscontinued() {
 		return discontinued;
 	}
 
-	public void setDiscontinued(String discontinued) {
+	public void setDiscontinued(LocalDate discontinued) {
 		this.discontinued = discontinued;
 	}
 
-	public int getCompanyId() {
-		return companyId;
+	public String getCompany() {
+		return company;
 	}
 
-	public void setCompanyId(int companyId) {
-		this.companyId = companyId;
+	public void setCompany(String company) {
+		this.company = company;
 	}
 
+	@Override
+	public String toString() {
+		return "Computer [id=" + id + ", name=" + name + ", introduced=" + introduced + ", discontinued=" + discontinued
+				+ ", company=" + company + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id != other.id)
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
