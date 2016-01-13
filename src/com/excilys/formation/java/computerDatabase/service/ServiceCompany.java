8f5190fc38605d0935c3d49d1718afc9e273b038
TimotheePapin <papin.timothee@gmail.com>
2016-01-13 15:48:52 +0100

    Fixed Services

--- src/com/excilys/formation/java/computerDatabase/service/ServiceCompany.java
+++ src/com/excilys/formation/java/computerDatabase/service/ServiceCompany.java
@@ -3,40 +3,24 @@ package com.excilys.formation.java.computerDatabase.service;
 import java.io.Serializable;
 import java.util.List;
 
-import com.excilys.formation.java.computerDatabase.mapper.MapCompany;
 import com.excilys.formation.java.computerDatabase.model.Company;
 import com.excilys.formation.java.computerDatabase.persistence.DaoCompany;
-import com.excilys.formation.java.computerDatabase.persistence.impl.DAOCompany;
 import com.excilys.formation.java.computerDatabase.persistence.impl.DAOCompanyImpl;
 
 @SuppressWarnings("serial")
 public class ServiceCompany implements Serializable {
 
 	private static ServiceCompany _instance = null;
-	private static DaoCompany sqlCompany;
+	private static DaoCompany daoCompany;
 
 	private ServiceCompany() {
-		sqlCompany = DAOCompanyImpl.getInstance();
+		daoCompany = DAOCompanyImpl.getInstance();
 	}
 
-	public void afficherCompany(Company company) {
-		System.out.println(company.getId() + " - " + company.getName());
+	public List<Company> getCompanies() {
+		return daoCompany.getAll();
 	}
 
-	public void afficherCompanies(List<Company> companies) {
-		for (Company company : companies) {
-			afficherCompany(company);
-		}
-	}
-
-	public List<Company> recupererCompanies() {
-		return mapCompany.mapcompanies(sqlCompany.recupCompanies());
-	}
-
-	public void closeConnection() {
-		sqlCompany.fermeConnection();
-	}
-	
 	synchronized public static ServiceCompany getInstance() {
 		if (_instance == null) {
 			_instance = new ServiceCompany();
--- src/com/excilys/formation/java/computerDatabase/service/ServiceComputer.java
+++ src/com/excilys/formation/java/computerDatabase/service/ServiceComputer.java
@@ -1,13 +1,8 @@
 package com.excilys.formation.java.computerDatabase.service;
 
 import java.io.Serializable;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.util.List;
-import java.util.Scanner;
 
-import com.excilys.formation.java.computerDatabase.mapper.MapComputer;
 import com.excilys.formation.java.computerDatabase.model.Computer;
 import com.excilys.formation.java.computerDatabase.persistence.impl.DAOComputerImpl;
 
@@ -15,199 +10,38 @@ import com.excilys.formation.java.computerDatabase.persistence.impl.DAOComputerI
 public class ServiceComputer implements Serializable {
 
 	private static ServiceComputer _instance = null;
-
-	private static DAOComputerImpl sqlComputer;
-
-	Scanner sc = new Scanner(System.in);
+	private static DAOComputerImpl daoComputer;
 
 	private ServiceComputer() {
-		sqlComputer = DAOComputerImpl.getInstance();
+		daoComputer = DAOComputerImpl.getInstance();
 	}
 
-	public void afficherComputer(Computer computer) {
-		System.out.println(computer.getId() + " - " + computer.getName() + " : " + computer.getIntroduced() + " ; "
-				+ computer.getDiscontinued() + " ; " + computer.getCompanyId());
+	public List<Computer> getAll() {
+		return daoComputer.getAll();
 	}
 
-	public void afficherComputers(List<Computer> computers) {
-		for (Computer computer : computers) {
-			afficherComputer(computer);
-		}
-		computers.size();
+	public Computer getByName(String name) {
+		return daoComputer.getByName(name);
 	}
 
-	public List<Computer> recupererComputers() {
-		return mapComputer.mapcomputers(sqlComputer.recupComputers());
+	public Computer getById(int id) {
+		return daoComputer.getById(id);
 	}
 
-	public Computer recupererComputer () {
-		ResultSet result;
-		System.out.println("Get Computer : \n 1-By name \n 2-By id \n 3-Return to Menu");
-		int rep = sc.nextInt();
-		if (rep == 1) {
-			System.out.println("Enter the name of the computer :");
-			sc.nextLine();
-			String name = sc.nextLine();
-			result  = sqlComputer.recupComputer(name);
-		}
-		else if (rep == 2) {
-			System.out.println("Enter the id of the computer:");
-			int id = sc.nextInt();
-			result  = sqlComputer.recupComputer(id);
-		}
-		else {
-			return null;
-		}
-		try {
-			if (result.wasNull()) {
-				System.out.println("This computer isn't in the database.");
-				return null;
-			}
-			else{
-				return mapComputer.mapcomputer(result);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
+	public void deleteByName(String name) {
+		daoComputer.deleteByName(name);
 	}
-	
-	public void supprimerComputer () {
-		System.out.println("Delete Computer : \n 1-By name \n 2-By id \n 3-Return to Menu");
-		int rep = sc.nextInt();
-		if (rep == 1) {
-			System.out.println("Enter the name of the computer :");
-			sc.nextLine();
-			String name = sc.nextLine();
-			sqlComputer.supprComputer(name);
-		}
-		else if (rep == 2) {
-			System.out.println("Enter the id of the computer :");
-			int id = sc.nextInt();
-			sqlComputer.supprComputer(id);
-		}
+
+	public void deleteById(int id) {
+		daoComputer.deleteById(id);
 	}
-	
-	@SuppressWarnings("deprecation")
-	public void creerComputer() {
-		Timestamp introduced=null;
-		Timestamp discontinued=null;
-		boolean date = false;
-		System.out.println("Enter Computer name :");
-		String name = sc.nextLine();
-		do {
-			try {
-				System.out.println("Enter Computer introduced (yyyy-mm-dd) :");
-				String introducedString = sc.nextLine();
-				String[] temp = introducedString.split("-");
-				introduced = new Timestamp(Integer.parseInt(temp[0])-1900, Integer.parseInt(temp[1])-1, Integer.parseInt(temp[2]), 0, 0, 0, 0);
-				System.out.println("Enter Computer discontinued (yyyy-mm-dd) :");
-				String discontinuedString = sc.nextLine();
-				temp = discontinuedString.split("-");
-				discontinued = new Timestamp(Integer.parseInt(temp[0])-1900, Integer.parseInt(temp[1])-1, Integer.parseInt(temp[2]), 0, 0, 0, 0);
-				date =true;
-			} catch (Exception e){ 
-				System.out.println("Wrong entry for one of the dates :");
-			}
-		}while (!date);
-		System.out.println("Enter Computer companyId :");
-		int companyId = sc.nextInt();
-		sqlComputer.creerComputer(name,introduced,discontinued,companyId);
+
+	public void create(Computer computer) {
+		daoComputer.add(computer);
 	}
-	
-	@SuppressWarnings("deprecation")
-	public void majComputer () {
-		ResultSet result;
-		int res=0;
-		boolean date =false;
-		Computer computer;
-		Timestamp introduced=null;
-		Timestamp discontinued=null;
-		System.out.println("Update Computer : \n 1-By name \n 2-By id \n 3-Return to Menu");
-		int rep = sc.nextInt();
-		if (rep == 1) {
-			System.out.println("Enter the name of the computer :");
-			sc.nextLine();
-			String name = sc.nextLine();
-			result  = sqlComputer.recupComputer(name);
-		}
-		else if (rep == 2) {
-			System.out.println("Enter the id of the computer :");
-			int id = sc.nextInt();
-			result  = sqlComputer.recupComputer(id);
-		}
-		else {
-			return;
-		}
-		try {
-			if (result.wasNull()) {
-				System.out.println("This computer isn't in the database.");
-			}
-			else{
-				computer = mapComputer.mapcomputer(result);
-				afficherComputer(computer);
-				String introducedString = computer.getIntroduced();
-				String[] temp = introducedString.split("-");
-				introduced = new Timestamp(Integer.parseInt(temp[0])-1900, Integer.parseInt(temp[1])-1, Integer.parseInt(temp[2].substring(0, 1)), 0, 0, 0, 0);
-				String discontinuedString = computer.getDiscontinued();
-				temp = discontinuedString.split("-");
-				discontinued = new Timestamp(Integer.parseInt(temp[0])-1900, Integer.parseInt(temp[1])-1, Integer.parseInt(temp[2].substring(0, 1)), 0, 0, 0, 0);
-				while (res != 4) {
-					System.out.println(
-							"Update : \n 1-Introduced \n 2-Discontinued \n 3-ComapnyId \n 4-Finish the update");
-					res=sc.nextInt();
-					switch (res) {
-					case 1:
-						date=false;
-						do {
-							try {
-								System.out.println("Enter the new value (yyyy-mm-dd) :");
-								sc.nextLine();
-								introducedString = sc.nextLine();
-								temp = introducedString.split("-");
-								introduced = new Timestamp(Integer.parseInt(temp[0])-1900, Integer.parseInt(temp[1])-1, Integer.parseInt(temp[2]), 0, 0, 0, 0);
-								date=true;
-							} catch (Exception e){ 
-								System.out.println("Wrong entry for the date :");
-							}
-						}while (!date);
-								break;
-					case 2:
-						date=false;
-						do {
-							try {
-								System.out.println("Enter the new value (yyyy-mm-dd):");
-								sc.nextLine();
-								discontinuedString = sc.nextLine();
-								temp = discontinuedString.split("-");
-								discontinued = new Timestamp(Integer.parseInt(temp[0])-1900, Integer.parseInt(temp[1])-1, Integer.parseInt(temp[2]), 0, 0, 0, 0);
-								date=true;
-							} catch (Exception e){ 
-								System.out.println("Wrong entry for the date :");
-							}
-						}while (!date);
-						break;
-					case 3:
-						System.out.println("Enter the new value :");
-						computer.setCompanyId(sc.nextInt());
-						break;
-					case 4 :
-						break;
-					default:
-						System.out.println("Incorrect entry");
-					}
-				}
-				sqlComputer.updateComputer(computer.getId(),computer.getName(),introduced,discontinued,computer.getCompanyId());
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void closeConnection() {
-		sqlComputer.fermeConnection();
+
+	public void update(Computer computer) {
+		daoComputer.update(computer);
 	}
 
 	synchronized public static ServiceComputer getInstance() {
