da37c4b9da93f98942815cae89c2e2e36604c6bf
TimotheePapin <papin.timothee@gmail.com>
2016-02-04 10:35:36 +0100

    No Selenium

--- src/main/java/com/excilys/formation/java/computerdatabase/persistence/DatabaseConnection.java
+++ src/main/java/com/excilys/formation/java/computerdatabase/persistence/DatabaseConnection.java
@@ -113,8 +113,6 @@ public class DatabaseConnection {
 			config.setMinConnectionsPerPartition(MINCONNECTION);
 			config.setMaxConnectionsPerPartition(MAXCONNECTION);
 			config.setPartitionCount(PARTITIONS);
-			config.setCloseConnectionWatch(true);
-			config.setCloseConnectionWatchTimeoutInMs(0);
 			connectionPool = new BoneCP(config);
 		} catch (SQLException e) {
 			LOGGER.error("Configuration error of the connection pool.");
